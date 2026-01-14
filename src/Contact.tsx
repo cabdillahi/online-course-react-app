6f21435526c4972eaea4daf9da9811adabdf57f2
Hamza Mustafa <hmustafe299@gmail.com>
2026-01-14 16:53:49 +0300

    Hello world

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import AppContent from './components/AppContent'
+import React from "react";
+import AppContent from "./components/AppContent";
 
 const Contact = () => {
   return (
     <div>
-        <AppContent title='Contact Us' description='Contact Page'/>
+      <AppContent title="Hello World" description="Contact Page" />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
